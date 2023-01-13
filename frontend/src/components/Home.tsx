1956d23c153a615eb9c72c5efdacdafe15fd9274
Kazuki Taira <kazuki@lift99.co>
2023-01-13 20:35:23 +0200

    promise all

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 const Home: React.FC = () => {
+
+    useEffect(() => {
+        const baseURL = 'https://hacker-news.firebaseio.com/v0'
+
+        fetch(`${baseURL}/topstories.json`)
+        .then(response => response.json())
+        .then((data: number[]) => {
+            const promises = data.slice(0, 100).map(id => fetch(`${baseURL}/item/${id}.json`))
+            return Promise.all(promises)
+        })
+        .then(response => Promise.all(response.map(r => r.json())))
+        .then(data => console.log(data))
+
+    }, [])
+
     return (
         <div>
-        <h1>Home</h1>
+            <h1>Home</h1>
         </div>
     )
 }
